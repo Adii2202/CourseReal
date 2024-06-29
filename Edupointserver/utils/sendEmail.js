1acd4b1afdef8a3e9806b17167dfb37c4e03a6cc
Adii2202 <adiningule2@gmail.com>
2024-06-29 21:40:51 +0530

    own mail server added

--- Edupointserver/utils/sendEmail.js
+++ Edupointserver/utils/sendEmail.js
@@ -2,8 +2,8 @@ import { createTransport } from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
   const transporter = createTransport({
-    host: "65.0.178.19", // Public IP of your EC2 instance
-    port: 25, // Port your SMTP server is listening on
+    host: "13.201.104.94", // Public IP of your EC2 instance
+    port: 2500, // Port your SMTP server is listening on
     secure: false, // Set to true if using SSL/TLS
     tls: {
       rejectUnauthorized: false, // This is for testing, in production, handle this properly
